1b52d51f82f2eb8ba6a07f967062c12e42312157
Amandeep Singh <aman.harry2744@gmail.com>
2022-03-12 00:29:36 +0530

    learning go(array slice map)

---
+++ 030/main.go
@@ -0,0 +1,28 @@
+// Using the code from the previous example, delete a record from your map. Now print the map
+// out using the “range” loop
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+
+	// fmt.Println(m)
+
+	m[`fleming_ian`] = []string{`steaks`, `cigars`, `espionage`}
+
+	delete(m, `no_dr`)
+
+	for k, v := range m {
+		fmt.Println("This is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+}
