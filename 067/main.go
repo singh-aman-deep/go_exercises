d168a033ca797e35fd6e56e2c3081b4fdc9b7068
Amandeep Singh <aman.harry2744@gmail.com>
2022-03-25 23:36:18 +0530

    Learning Go (Error Handling)

---
+++ 067/main.go
@@ -0,0 +1,28 @@
+// Create a struct “customErr” which implements the builtin.error interface. Create a func “foo” that
+// has a value of type error as a parameter. Create a value of type “customErr” and pass it into
+// “foo”
+package main
+
+import (
+	"fmt"
+)
+
+type customErr struct {
+	info string
+}
+
+func (ce customErr) Error() string {
+	return fmt.Sprintf("here is the error: %v", ce.info)
+}
+
+func main() {
+	c1 := customErr{
+		info: "need more coffee",
+	}
+
+	foo(c1)
+}
+
+func foo(e error) {
+	fmt.Println("foo ran -", e, "\n")
+}
